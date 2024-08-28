7c7ea812218e3699e5699ec17e69f36389a25a49
Rohit Nandi <rohitnandi12@gmail.com>
2024-08-28 14:31:21 +0530

    scaler:day17:Sorting:ArrayWithConsecutiveElements

---
+++ src/com/scaler/sorting/ArrayWithConsecutiveElements.java
@@ -0,0 +1,61 @@
+package com.scaler.sorting;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Problem Description
+ * Given an array of positive integers A, check and return whether the array elements are consecutive or not.
+ *
+ * NOTE: Try this with O(1) extra space.
+ *
+ * Problem Constraints
+ * 1 <= length of the array <= 105
+ * 1 <= A[i] <= 109
+ *
+ * Input Format
+ * The only argument given is the integer array A.
+ *
+ * Output Format
+ * Return 1 if the array elements are consecutive else return 0.
+ *
+ * Example Input
+ * Input 1:
+ *
+ *  A = [3, 2, 6, 4, 5]
+ * Input 2:
+ *
+ *  A = [1, 3, 2, 5]
+ *
+ * Example Output
+ * Output 1:
+ *
+ *  1
+ * Output 2:
+ *
+ *  0
+ *
+ * Example Explanation
+ * Explanation 1:
+ *
+ *  As you can see all the elements are consecutive (i.e 2, 3, 4, 5, 6), so we return 1.
+ * Explanation 2:
+ *
+ *  Element 4 is missing, so we return 0.
+ */
+public class ArrayWithConsecutiveElements {
+
+    public static void main(String[] args) {
+        System.out.println(solve(new ArrayList<>(Arrays.asList(3, 2, 6, 4, 5)))); // True or 1
+        System.out.println(solve(new ArrayList<>(Arrays.asList(1, 3, 2, 5)))); // False or 0
+    }
+    public static int solve(ArrayList<Integer> A) {
+        Collections.sort(A);
+        for(int i=0; i<A.size()-1; i++) {
+            if(A.get(i) + 1 != A.get(i+1))
+                return 0;
+        }
+        return 1;
+    }
+}
